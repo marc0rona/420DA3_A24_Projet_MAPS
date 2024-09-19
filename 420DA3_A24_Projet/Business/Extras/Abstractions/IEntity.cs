dd1912b13d1e1d915ad13bd5d8855b59f4848cda
Marc-Eric Boury <meboury@hotmail.com>
2024-09-19 15:42:45 -0400

    Ajout d'une interface (EXTRA) pour les entités du domaine

---
+++ 420DA3_A24_Projet/Business/Extras/Abstractions/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Extras.Abstractions;
+public interface IEntity<TIdentifier>
+{
+
+    TIdentifier? GetId();
+    DateTime? GetDateCreated();
+    DateTime? GetDateModified();
+    DateTime? GetDateDeleted();
+    bool IsDeleted();
+
+}
