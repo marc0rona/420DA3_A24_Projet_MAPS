d1d7308769db6617f13d32a5c5e428619ebbeeb0
Marc-Eric Boury <meboury@hotmail.com>
2024-10-29 16:57:17 -0400

    entités User et Role

--- 420DA3_A24_Projet/Business/Domain/Role.cs
+++ 420DA3_A24_Projet/Business/Domain/Role.cs
@@ -1,25 +1,92 @@
-﻿using ExtraAdvancedMultiTier.Business.Abstractions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
-public class Role : AbstractEntity<int> {
+public class Role {
+    public const int ADMIN_ROLE_ID = 1;
+    public const int OFFICE_EMPLOYEE_ROLE_ID = 2;
+    public const int WAREHOUSE_EMPLOYEE_ROLE_ID = 3;
+    public const int NAME_MAX_LENGTH = 64;
+    public const int DESCRIPTION_MAX_LENGTH = 255;
 
-    // L'utilisation des structures avancées fournies (ici l'héritage de AbstractEntity<int>)
-    // ajoute automatiquement une propriété Id de type int,
-    // des propriétés DateCreated, DateModified et DateDeleted de type DateTime,
-    // et une propriété RowVersion de type byte[].
-    // Je n'ai donc pas besoin de les ajouter ici vu qu'elles sont héritées
+    private int id;
+    private string name = null!;
+    private string description = null!;
 
-    // propriétés de données
-    public string RoleName { get; set; }
-    public string RoleDescription { get; set; }
 
-    // propriétés de navigation
-    public List<User> RoleUsers { get; set; } = new List<User>();
+    public int Id {
+        get { return this.id; }
+        set {
+            if (!ValidateId(value)) {
+                throw new ArgumentOutOfRangeException("Id", "Id must be greater than or equal to 0.");
+            }
+            this.id = value;
+        }
+    }
+    public string Name {
+        get { return this.name; }
+        set {
+            if (!ValidateName(value)) {
+                throw new ArgumentOutOfRangeException("Name", $"Name length must be lower than or equal to {NAME_MAX_LENGTH} characters.");
+            }
+            this.name = value;
+        }
+    }
+    public string Description {
+        get { return this.description; }
+        set {
+            if (!ValidateName(value)) {
+                throw new ArgumentOutOfRangeException("Description", $"Description length must be lower than or equal to {DESCRIPTION_MAX_LENGTH} characters.");
+            }
+            this.description = value;
+        }
+    }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateModified { get; set; }
+    public DateTime? DateDeleted { get; set; }
+    public byte[] RowVersion { get; set; } = null!;
 
-    public Role(string roleName, string roleDescription) {
-        this.RoleName = roleName;
-        this.RoleDescription = roleDescription;
+
+    public virtual List<User> Users { get; set; } = new List<User>();
+
+
+
+    public Role(string name, string description) {
+        this.Name = name;
+        this.Description = description;
     }
 
-    public Role(int id, string roleName, string roleDescription, DateTime dateCreated, DateTime? dateModified, DateTime? dateDeleted, byte[] rowVersion) { }
+    protected Role(int id,
+        string name,
+        string description,
+        DateTime dateCreated,
+        DateTime? dateModified,
+        DateTime? dateDeleted,
+        byte[] rowVersion)
+        : this(name, description) {
+
+        this.Id = id;
+        this.DateCreated = dateCreated;
+        this.DateModified = dateModified;
+        this.DateDeleted = dateDeleted;
+        this.RowVersion = rowVersion;
+    }
+
+
+
+    public static bool ValidateId(int id) {
+        return id >= 0;
+    }
+
+    public static bool ValidateName(string name) {
+        return name.Length <= NAME_MAX_LENGTH;
+    }
+
+    public static bool ValidateDescription(string description) {
+        return description.Length <= DESCRIPTION_MAX_LENGTH;
+    }
+
 }
--- 420DA3_A24_Projet/Business/Domain/User.cs
+++ 420DA3_A24_Projet/Business/Domain/User.cs
@@ -1,25 +1,62 @@
-﻿namespace _420DA3_A24_Projet.Business.Domain;
-public class User {
-    public const int UsernameMaxLength = 64;
-    public const int PasswordHashMaxLength = 128;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    // Propriétés de données
-    public int Id { get; set; }
+namespace _420DA3_A24_Projet.Business.Domain;
+public class User {
+    public const int USERNAME_MIN_LENGTH = 4;
+    public const int USERNAME_MAX_LENGTH = 64;
+    public const int PASSWORDHASH_MAX_LENGTH = 128;
+    public const int PASSWORD_MIN_LENGTH = 8;
+    public const int PASSWORD_MAX_LENGTH = 32;
+
+    // backing fields
+    private int id;
+    private string username = null!;
+    private string passwordHash = null!;
+
+    // propriétés de données
+    public int Id { 
+        get { return this.id; } 
+        set {
+            if (!ValidateId(value)) {
+                throw new ArgumentOutOfRangeException("Id", "Id must be greater than or equal to 0.");
+            }
+            this.id = value;
+        } 
+    }
+    public string Username { 
+        get { return this.username; } 
+        set {
+            if (!ValidateUsername(value)) {
+                throw new ArgumentOutOfRangeException("Username", $"Username length must be greater than or equal to {USERNAME_MIN_LENGTH} characters and lower than or equal to {USERNAME_MAX_LENGTH} characters.");
+            }
+            this.username = value;
+        } 
+    }
+    public string PasswordHash {
+        get { return this.passwordHash; }
+        set {
+            if (!ValidatePasswordHash(value)) {
+                throw new ArgumentOutOfRangeException("PasswordHash", $"PasswordHash length must be lower than or equal to {PASSWORDHASH_MAX_LENGTH} characters.");
+            }
+            this.passwordHash = value;
+        }
+    }
     public int? EmployeeWarehouseId { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime? DateModified { get; set; }
     public DateTime? DateDeleted { get; set; }
     public byte[] RowVersion { get; set; } = null!;
 
-    // propriétés de navigation
+    // Propriétés de navigation
     public virtual List<Role> Roles { get; set; } = new List<Role>();
+    //public virtual List<ShippingOrder> CreatedShippingOrders { get; set; } = new List<ShippingOrder>();
+    //public virtual List<ShippingOrder> FulfilledShippingOrders { get; set; } = new List<ShippingOrder>();
+    //public virtual Warehouse? EmployeeWarehouse { get; set; };
 
-    // TODO: @PROF nav props for
-    // - created shipping orders
-    // - fulfilled shipping orders
-    // - Employee warehouse
 
     public User(string username, string passwordHash, int? employeeWarehouseId = null) {
         this.Username = username;
@@ -27,14 +64,15 @@ public class User {
         this.EmployeeWarehouseId = employeeWarehouseId;
     }
 
-    protected User(int id,
-        string username,
-        string passwordHash,
-        int? employeeWarehouseId,
-        DateTime dateCreated,
-        DateTime? dateModified,
-        DateTime? dateDeleted,
-        byte[] rowVersion) : this(username, passwordHash, employeeWarehouseId) {
+    protected User(int id, 
+        string username, 
+        string passwordHash, 
+        int? employeeWarehouseId, 
+        DateTime dateCreated, 
+        DateTime? dateModified, 
+        DateTime? dateDeleted, 
+        byte[] rowVersion)
+        : this(username, passwordHash, employeeWarehouseId) {
 
         this.Id = id;
         this.DateCreated = dateCreated;
@@ -43,4 +81,40 @@ public class User {
         this.RowVersion = rowVersion;
     }
 
+
+    public static bool ValidateId(int id) {
+        return id >= 0;
+    }
+
+    public static bool ValidateUsername(string username) {
+        return username.Length >= USERNAME_MIN_LENGTH 
+            && username.Length <= USERNAME_MAX_LENGTH;
+    }
+
+    public static bool ValidatePasswordHash(string passwordHash) {
+        return passwordHash.Length <= PASSWORDHASH_MAX_LENGTH;
+    }
+
+    public static bool ValidatePassword(string password) {
+        return password.Length >= PASSWORD_MIN_LENGTH
+            && password.Length <= PASSWORD_MAX_LENGTH;
+    }
+
+    public bool IsAdministrator() {
+        return this.Roles.Any(role => {
+            return role.Id == Role.ADMIN_ROLE_ID;
+        });
+    }
+
+    public bool IsOfficeEmployee() {
+        return this.Roles.Any(role => {
+            return role.Id == Role.OFFICE_EMPLOYEE_ROLE_ID;
+        });
+    }
+
+    public bool IsWarehouseEmployee() {
+        return this.Roles.Any(role => {
+            return role.Id == Role.WAREHOUSE_EMPLOYEE_ROLE_ID;
+        });
+    }
 }
