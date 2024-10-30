ea52f92d663901a76b145a892b6cc8422bd02908
Marco <marc0mourr96@gmail.com>
2024-10-30 10:53:33 -0400

    Added Product & Supplier class

---
+++ 420DA3_A24_Projet/Business/Domain/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Product
+{
+	public Product()
+	{
+		//Strings LIMIT    
+        public const Int32 MAX_LENGHT_NAME = 64;
+        public const Int32 MAX_LENGHT_DESC = 600;
+        public const Int32 MAX_LENGHT_CODEUPC = 6;
+
+        //PRIVATE Fields - Champs
+        private string nomVerif = "";
+        private string prenomVerif = "";
+        private string codePermVerif = "";
+        //Initialisation de la Liste COURS  
+        private List<Cours> coursL = new List<Cours>();
+
+        //Fields - Champs 
+        public int Id { get; set; }
+    }
+}
---
+++ 420DA3_A24_Projet/Business/Domain/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Supplier
+{
+	public Supplier()
+	{
+		//Strings LIMIT    
+        public const Int32 MAX_LENGHT_NAME = 64;
+        public const Int32 MAX_LENGHT_EMAIL = 25;
+        public const Int32 MAX_LENGHT_PHONE = 11;
+
+        //Fields - Champs 
+        public int Id { get; set; }
+	}
+}
