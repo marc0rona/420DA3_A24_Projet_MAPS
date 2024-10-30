df15a0fb61fd91e1a60263939c6d97033e67784f
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-10-30 11:02:26 -0400

    setup services avancés et exemple pour patient

--- ExtraAdvancedMultiTier/Business/Abstractions/AbstractEntityService.cs
+++ ExtraAdvancedMultiTier/Business/Abstractions/AbstractEntityService.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ExtraAdvancedMultiTier.Business.Abstractions.Daos;
 
 namespace ExtraAdvancedMultiTier.Business.Abstractions;
-public abstract class AbstractEntityService<TEntity, TEntityKey> : AbstractService 
-    where TEntity : class, IEntity<TEntityKey> 
+public abstract class AbstractEntityService<TEntity, TEntityKey> : AbstractService, IEntityService<TEntity, TEntityKey>
+    where TEntity : class, IEntity<TEntityKey>
     where TEntityKey : IEquatable<TEntityKey> {
 
-    protected AbstractEntityService(IServiceContainer parent) : base(parent) { }
+    protected AbstractEntityService(IServiceContainer parent)
+        : base(parent) { }
 
 
+    public abstract IDao<TEntity, TEntityKey> Dao { get; }
+
+    public TEntity Create(TEntity entity) {
+        return this.Dao.Create(entity);
+    }
+
+    public List<TEntity> GetAll() {
+        return this.Dao.GetAll();
+    }
+
+    public TEntity? GetById(TEntityKey id) {
+        return this.Dao.GetById(id);
+    }
+
+    public TEntity Update(TEntity entity) {
+        return this.Dao.Update(entity);
+    }
+
+    public TEntity Delete(TEntity entity, bool softDeletes = true) {
+        return this.Dao.Delete(entity, softDeletes);
+    }
 }
---
+++ ExtraAdvancedMultiTier/Business/Abstractions/IEntityView.cs
@@ -0,0 +1,11 @@
+﻿using Project_Utilities.Enums;
+
+namespace ExtraAdvancedMultiTier.Business.Abstractions;
+public interface IEntityView<TEntity, TEntityKey>
+    where TEntity : class, IEntity<TEntityKey>
+    where TEntityKey : IEquatable<TEntityKey> {
+
+    protected abstract IEntityService<TEntity, TEntityKey> Service { get; }
+    protected TEntity? CurrentInstance { get; set; }
+    protected ViewActionsEnum CurrentAction { get; set; }
+}
