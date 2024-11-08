3f242529c902779425d9d2d026561b6ac02c73ed
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-08 16:55:06 -0500

    Utilisation de nameof dans config de colonnes d'entités

--- 420DA3_A24_Projet/DataAccess/Contexts/AppDbContext.cs
+++ 420DA3_A24_Projet/DataAccess/Contexts/AppDbContext.cs
@@ -25,14 +25,14 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.Id)
-            .HasColumnName("Id")
+            .HasColumnName(nameof(User.Id))
             .HasColumnOrder(0)
             .HasColumnType("int")
             .UseIdentityColumn(1, 1);
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.Username)
-            .HasColumnName("Username")
+            .HasColumnName(nameof(User.Username))
             .HasColumnOrder(1)
             .HasColumnType($"nvarchar({User.USERNAME_MAX_LENGTH})")
             .HasMaxLength(User.USERNAME_MAX_LENGTH)
@@ -40,7 +40,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.PasswordHash)
-            .HasColumnName("PasswordHash")
+            .HasColumnName(nameof(User.PasswordHash))
             .HasColumnOrder(2)
             .HasColumnType($"nvarchar({User.PASSWORDHASH_MAX_LENGTH})")
             .HasMaxLength(User.PASSWORDHASH_MAX_LENGTH)
@@ -48,14 +48,14 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.EmployeeWarehouseId)
-            .HasColumnName("EmployeeWarehouseId")
+            .HasColumnName(nameof(User.EmployeeWarehouseId))
             .HasColumnOrder(3)
             .HasColumnType("int")
             .IsRequired(false);
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.DateCreated)
-            .HasColumnName("DateCreated")
+            .HasColumnName(nameof(User.DateCreated))
             .HasColumnOrder(4)
             .HasColumnType("datetime2")
             .HasPrecision(7)
@@ -64,7 +64,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.DateModified)
-            .HasColumnName("DateModified")
+            .HasColumnName(nameof(User.DateModified))
             .HasColumnOrder(5)
             .HasColumnType("datetime2")
             .HasPrecision(7)
@@ -72,7 +72,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.DateDeleted)
-            .HasColumnName("DateDeleted")
+            .HasColumnName(nameof(User.DateDeleted))
             .HasColumnOrder(6)
             .HasColumnType("datetime2")
             .HasPrecision(7)
@@ -80,7 +80,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<User>()
             .Property(user => user.RowVersion)
-            .HasColumnName("RowVersion")
+            .HasColumnName(nameof(User.RowVersion))
             .HasColumnOrder(7)
             .IsRowVersion();
 
@@ -110,7 +110,6 @@ internal class AppDbContext : DbContext {
 
         #region ROLE
 
-        // TODO: @PROF Faire config Role
         _ = modelBuilder.Entity<Role>()
             .ToTable(nameof(this.Roles))
             .HasKey(role => role.Id);
@@ -121,14 +120,14 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.Id)
-            .HasColumnName("Id")
+            .HasColumnName(nameof(Role.Id))
             .HasColumnOrder(0)
             .HasColumnType("int")
             .UseIdentityColumn(1, 1);
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.Name)
-            .HasColumnName("Name")
+            .HasColumnName(nameof(Role.Name))
             .HasColumnOrder(1)
             .HasColumnType($"nvarchar({Role.NAME_MAX_LENGTH})")
             .HasMaxLength(Role.NAME_MAX_LENGTH)
@@ -136,7 +135,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.Description)
-            .HasColumnName("Description")
+            .HasColumnName(nameof(Role.Description))
             .HasColumnOrder(2)
             .HasColumnType($"nvarchar({Role.DESCRIPTION_MAX_LENGTH})")
             .HasMaxLength(Role.DESCRIPTION_MAX_LENGTH)
@@ -144,7 +143,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.DateCreated)
-            .HasColumnName("DateCreated")
+            .HasColumnName(nameof(Role.DateCreated))
             .HasColumnOrder(3)
             .HasColumnType("datetime2")
             .HasPrecision(7)
@@ -153,7 +152,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.DateModified)
-            .HasColumnName("DateModified")
+            .HasColumnName(nameof(Role.DateModified))
             .HasColumnOrder(4)
             .HasColumnType("datetime2")
             .HasPrecision(7)
@@ -161,7 +160,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.DateDeleted)
-            .HasColumnName("DateDeleted")
+            .HasColumnName(nameof(Role.DateDeleted))
             .HasColumnOrder(5)
             .HasColumnType("datetime2")
             .HasPrecision(7)
@@ -169,7 +168,7 @@ internal class AppDbContext : DbContext {
 
         _ = modelBuilder.Entity<Role>()
             .Property(role => role.RowVersion)
-            .HasColumnName("RowVersion")
+            .HasColumnName(nameof(Role.RowVersion))
             .HasColumnOrder(6)
             .IsRowVersion();
 
