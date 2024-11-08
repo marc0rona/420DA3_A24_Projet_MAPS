a1dff54459e15c0d71521d69928ac4f265353cc4
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-08 16:55:29 -0500

    Ajout de UserDAO et RoleDAO

---
+++ 420DA3_A24_Projet/DataAccess/DAOs/RoleDAO.cs
@@ -0,0 +1,107 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace _420DA3_A24_Projet.DataAccess.DAOs;
+
+/// <summary>
+/// TODO @PROF : documenter
+/// </summary>
+internal class RoleDAO {
+
+    private readonly AppDbContext context;
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="context"></param>
+    public RoleDAO(AppDbContext context) {
+        this.context = context;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public Role? GetById(int id, bool includeDeleted = false) {
+        return this.context.Roles
+            .Where(role => role.Id == id && (includeDeleted || role.DateDeleted == null))
+            .Include(role => role.Users)
+            .SingleOrDefault();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="roleName"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public Role? GetByUsername(string roleName, bool includeDeleted = false) {
+        return this.context.Roles
+            .Where(role => role.Name == roleName && (includeDeleted || role.DateDeleted == null))
+            .Include(role => role.Users)
+            .SingleOrDefault();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="criterion"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public List<Role> Search(string criterion, bool includeDeleted = false) {
+        return this.context.Roles
+            .Where(role => (
+                role.Id.ToString().Contains(criterion)
+                || role.Name.ToLower().Contains(criterion.ToLower())
+                || role.Description.ToLower().Contains(criterion.ToLower())
+            ) && (includeDeleted || role.DateDeleted == null))
+            .Include(user => user.Users)
+            .ToList();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="role"></param>
+    /// <returns></returns>
+    public Role Create(Role role) {
+        _ = this.context.Roles.Add(role);
+        _ = this.context.SaveChanges();
+        return role;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="role"></param>
+    /// <returns></returns>
+    public Role Update(Role role) {
+        role.DateModified = DateTime.Now;
+        _ = this.context.Roles.Update(role);
+        _ = this.context.SaveChanges();
+        return role;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="role"></param>
+    /// <param name="softDeletes"></param>
+    /// <returns></returns>
+    public Role Delete(Role role, bool softDeletes = true) {
+        if (softDeletes) {
+            role.DateDeleted = DateTime.Now;
+            _ = this.context.Roles.Update(role);
+
+        } else {
+            _ = this.context.Roles.Remove(role);
+        }
+        _ = this.context.SaveChanges();
+        return role;
+    }
+
+
+}
---
+++ 420DA3_A24_Projet/DataAccess/DAOs/UserDAO.cs
@@ -0,0 +1,109 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace _420DA3_A24_Projet.DataAccess.DAOs;
+
+/// <summary>
+/// TODO @PROF : documenter
+/// </summary>
+internal class UserDAO {
+
+    private readonly AppDbContext context;
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="context"></param>
+    public UserDAO(AppDbContext context) {
+        this.context = context;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public User? GetById(int id, bool includeDeleted = false) {
+        return this.context.Users
+            .Where(user => user.Id == id && (includeDeleted || user.DateDeleted == null))
+            .Include(user => user.Roles)
+            .Include(user => user.EmployeeWarehouse)
+            .SingleOrDefault();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="username"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public User? GetByUsername(string username, bool includeDeleted = false) {
+        return this.context.Users
+            .Where(user => user.Username == username && (includeDeleted || user.DateDeleted == null))
+            .Include(user => user.Roles)
+            .Include(user => user.EmployeeWarehouse)
+            .SingleOrDefault();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="criterion"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
+    public List<User> Search(string criterion, bool includeDeleted = false) {
+        return this.context.Users
+            .Where(user => (
+                user.Id.ToString().Contains(criterion)
+                || user.Username.ToLower().Contains(criterion.ToLower())
+            ) && (includeDeleted || user.DateDeleted == null))
+            .Include(user => user.Roles)
+            .Include(user => user.EmployeeWarehouse)
+            .ToList();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public User Create(User user) {
+        _ = this.context.Users.Add(user);
+        _ = this.context.SaveChanges();
+        return user;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public User Update(User user) {
+        user.DateModified = DateTime.Now;
+        _ = this.context.Users.Update(user);
+        _ = this.context.SaveChanges();
+        return user;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="softDeletes"></param>
+    /// <returns></returns>
+    public User Delete(User user, bool softDeletes = true) {
+        if (softDeletes) {
+            user.DateDeleted = DateTime.Now;
+            _ = this.context.Users.Update(user);
+
+        } else {
+            _ = this.context.Users.Remove(user);
+        }
+        _ = this.context.SaveChanges();
+        return user;
+    }
+
+
+}
