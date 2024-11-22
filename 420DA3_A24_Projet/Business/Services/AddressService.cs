cbfbf0095806c5bb5d981af6113e0402de185067
P-S.Cypre <pscypre@gmail.com>
2024-11-22 09:19:50 -0500

    (Service + DAO) Ajout des classes Services et DAO pour Address and Shipments

---
+++ 420DA3_A24_Projet/Business/Services/AddressService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Services;
+internal class AddressService {
+}
