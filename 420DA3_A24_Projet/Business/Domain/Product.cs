752a4bb4efaa67fad3dffeab6b2e5a19edecf0ed
Marco <marc0mourr96@gmail.com>
2024-12-01 03:28:50 -0500

    Terminer les Domains Products + Suppliers (sauf navigations)

--- 420DA3_A24_Projet/Business/Domain/Product.cs
+++ 420DA3_A24_Projet/Business/Domain/Product.cs
@@ -20,8 +20,8 @@ public class Product
     private string ProdName_Verif = null!; //Non-nullable, nécessaire
     private string ProdDesc_Verif = null!;
     private string ProdCode_Verif = null!;
-    private string IMG_FileName_Verif;
-    private string SuppCode_Verif = null!;
+    private string IMG_FileName_Verif = null;
+    private string SuppCode_Verif = null;
     //Variables pour les quantités + poids (ne peuvent pas être négatives)
     private Int32 Qty_instock_Verif; //Non-nullable, nécessaire
     private Int32 Qty_desired_Verif;
@@ -33,33 +33,87 @@ public class Product
     {
         get { return this.id_Verif; } //prend variable privé
         set {   //En utilisant notre Methode, check si pas Positif (! + ValidateP). Si oui, throw ArgOutOfRangeEx( ParamName, Message )
-            if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Id", "Id is negative. Id must be greater than or equal to 0."); }
-            this.id_Verif = value; //< Sinon = valide le input 
+              if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Id", "Id is negative. Id must be greater than or equal to 0."); }
+              this.id_Verif = value; //< Sinon = valide le input 
         }
+    }
+    //1 : Validation Name
+    public string ProductName {
+        get { return this.ProdName_Verif; }
+        set {
+              if (!ValidateProductName(value)) { throw new ArgumentOutOfRangeException("Name", $"Name inputed is too long. Maximum number of characters allowed is {PROD_NAME_MAXLENGHT} chars.\nPlease respect the limit norms, thank you."); }
+              this.ProdName_Verif = value;
+            }
+    }
+    //2 : Validation Description
+    public string ProductDesc {
+        get { return this.ProdDesc_Verif; }
+        set {
+              if (!ValidateProductDescription(value)) { throw new ArgumentOutOfRangeException("Description", $"Description length must be lower than or equal to {PROD_DESCRIPTION_MAXLENGHT} characters.\nPlease respect the limit norms, thank you."); }
+              this.ProdDesc_Verif = value;
+            }
+    }
+    //3 : Validation code UPC du Produit
+    public string Product_CodeUPC {
+        get { return this.ProdCode_Verif; }
+        set { if (!ValidateUpCode(value)) { throw new ArgumentOutOfRangeException("Code UPC", $"The Code is too long. Maximum number of characters allowed is {UP_CODE_MAXLENGHT} chars.\nPlease respect the limit norms, thank you."); }
+              this.ProdCode_Verif = value;
+            }
+    } 
+    //4 : Validation code du Fournisseur
+    public string? Supplier_Code {
+        get { return this.SuppCode_Verif; }
+        set { if (!ValidateSupplier(value)) { throw new ArgumentOutOfRangeException("Supplier", $"Code length must be lower than or equal to {SUPPLIER_CODE_MAXLENGHT} characters.\nPlease respect the limit norms, thank you."); }
+              this.SuppCode_Verif = value;
+            }
+    }
+    //5 : Validation Nom de l'image (se rappeler que se peut etre NULL, > ToDo)
+    public string? Image_FileName {
+        get { return this.IMG_FileName_Verif; }
+        set { if (!ValidateImageFileName(value)) { throw new ArgumentOutOfRangeException("File Name", $"File Name is too long. Maximum number of characters allowed is {IMG_FILE_NAME_MAXLENGHT} chars.\nPlease respect the limit norms, thank you."); }
+              this.IMG_FileName_Verif = value;
+            }
     }
     //6 + 7 : Validation Quantités de Produits (en KG)
     public int Qty_InStock {
         get { return this.Qty_instock_Verif; }
         set {
-            if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Quantity", "Quantity is negative. A Quantity value must be greater than or equal to 0."); }
-            this.Qty_desired_Verif = value; //< Sinon = valide le input 
-        }
+              if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Quantity", "Quantity is negative. A Quantity value must be greater than or equal to 0."); }
+              this.Qty_desired_Verif = value; //< Sinon = valide le input 
+            }
     }
     public int Qty_Desired {
         get { return this.Qty_desired_Verif; }
         set {
-            if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Quantity", "Quantity is negative. A Quantity value must be greater than or equal to 0."); }
-            this.Qty_desired_Verif = value; //< Sinon = valide le input 
-        }
+              if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Quantity", "Quantity is negative. A Quantity value must be greater than or equal to 0."); }
+              this.Qty_desired_Verif = value; //< Sinon = valide le input 
+            }
     }
     //8 : Validation du poids en KG
     public double Weight_inKg {
         get { return this.weight_Verif; }
         set {
-            if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Weight", "The weight is negative. Weight value in kilograms must be greater than or equal to 0."); }
-            this.weight_Verif = value; //< Sinon = valide le input 
-        }
+              if (!ValidatePositiveNumber(value)) { throw new ArgumentOutOfRangeException("Weight", "The weight is negative. Weight value in kilograms must be greater than or equal to 0."); }
+              this.weight_Verif = value; //< Sinon = valide le input 
+            }
     }
+    //9 + 10 : Client du Produit - Infos
+    public int ProdOwnerClient_Id { get; set; }
+    public Client ProdOwnerClient { get; set; }
+    //11 + 12 : Fournisseur du Produit - Infos
+    public int ProdSupplier_Id { get; set; }
+    public Supplier ProdSupplier { get; set; } = null!; //doit avoir toujours un fournisseur
+
+    //13 - 15 : Variables Dates
+    public DateTime DateCreated { get; set; }
+    //Variables Nullables
+    public DateTime? DateModified { get; set; }
+    public DateTime? DateDeleted { get; set; }
+    //16 + 17 : LISTES pour gérer les expéditions reliés au produit & client
+    public List<PurchaseOrders> OrderPurchases { get; set; } = new List<PurchaseOrders>(); //CHANGE THE INTERNAL TO PUBLIC CLASS, ABOUBACAR PLEASE. THANK YOU
+    public List<ShipmentOrders> OrderShipments { get; set; } = new List<ShipmentOrders>();
+    //ToDo : Gerer si les expeditions sont vide (aucunes valeurs dans colonne);]
+    public byte[] RowVersion { get; set; } = null!; //Non-nullable, nécessaire
     #endregion 1 - Propriétés de données
 
     #region 2 - Propriétés de navigation
@@ -67,16 +121,51 @@ public class Product
     #endregion Propriétés de navigation
     /// Constructors
 
-    // Const Orienté Création Manuelle/UI par employé de bureau
-    public Product(int ID, string code, string nom, string description, string? IMG_nom, string supplier, Int32 Qty_1, Int32 Qty_2)  //(5 + 2 (Qntés) Arguments, valeurs qui sont assignés manuellements)
+    //1er Const : Orienté Création Manuelle/UI par employé de bureau
+    public Product(int ID, string nom, string description, string codeUPC, string? IMG_nom, int client_ID, int supplier_ID, string? supplier_code)  //(7 Arguments, valeurs qui sont assignés manuellements)
     {
-        // ToDo
+        this.Id = ID;
+        this.ProductName = nom;
+        this.ProductDesc = description;
+        this.Product_CodeUPC = codeUPC;
+        this.Image_FileName = IMG_nom;
 
+        this.ProdOwnerClient_Id = client_ID;
+        this.ProdSupplier_Id = supplier_ID;
+        this.Supplier_Code = supplier_code;
+    }
+    //2me Const : avec 3 arguments de plus (5 + 3 Arguments pour Qntés & poids)
+    public Product(int ID, string nom, string description, string codeUPC, string? IMG_nom, int client_ID, int supplier_ID, string? supplier_code,
+                   Int32 Qty_1, Int32 Qty_2, Double weight) : this(ID, nom, description, codeUPC, IMG_nom, client_ID, supplier_ID, supplier_code) //Reprend valeurs du 1er Const
+    {
+        this.Qty_InStock = Qty_1;
+        this.Qty_Desired = Qty_2;
+        this.Weight_inKg = weight;
     }
 
-    // ToDo : 2me Const (valeurs automaticaques created by DataBase SQL)
-
-    // ToDo : Const Orienté Entity-Framework
+    // 3me Const Orienté Entity-Framework + Valeurs automaticaques created by DataBase SQL (comme les DateTimes)
+    protected Product(int ID, // 0
+                      string nom,
+                      string description, // 2
+                      string codeUPC,
+                      string? IMG_nom, // 4
+                      int client_ID,
+                      int supplier_ID,// 6
+                      string? supplier_code,
+                      Int32 Qty_1, // 8
+                      Int32 Qty_2,
+                      Double weight, // 10      
+                        DateTime dateCreated,
+                        DateTime? dateModified,
+                        DateTime? dateDeleted,
+                        byte[] rowVersion)
+                        : this(ID, nom, description, codeUPC, IMG_nom, client_ID, supplier_ID, supplier_code, Qty_1, Qty_2, weight) //Reprend valeurs du 1er + 2me Consts
+    {
+        this.DateCreated = dateCreated;
+        this.DateModified = dateModified;
+        this.DateDeleted = dateDeleted;
+        this.RowVersion = rowVersion;
+    }
     #region 3 - MÉTHODES
     public static bool ValidatePositiveNumber(double numb) 
         { return numb >= 0; } //doit etre positif 
@@ -90,8 +179,8 @@ public class Product
         { return IMG.Length <= IMG_FILE_NAME_MAXLENGHT; }
     public static bool ValidateSupplier(string code) 
         { return code.Length <= SUPPLIER_CODE_MAXLENGHT; }
-
-    //ToDo : public static bool IsDueForRestocking() {  } 
+    public static bool IsDueForRestocking(int quantity_inStock) 
+        { return (quantity_inStock == 0); } //seulement retourne TRUE si la condition est appliqué (sinon FALSE) 
     #endregion
 }
 
--- 420DA3_A24_Projet/Business/Domain/Supplier.cs
+++ 420DA3_A24_Projet/Business/Domain/Supplier.cs
@@ -9,49 +9,62 @@ public class Supplier {
     //Limites pour les variables strings
     public const int SUPPLIER_NAME_MAXLENGHT = 128;
     public const int CONTACT_NAME_MAXLENGHT = 32;
+    public const int CONTACT_EMAIL_MAXLENGHT = 100;
     public const int CONTACT_PHONE_MAXLENGHT = 15;
-    public const int CONTACT_EMAIL_MAXLENGHT = 128;
 
     ///Champs (Field) : privés qui servent pour vérifier/valider les Limites
     private int id_Verif; //si id devient 'negatif'
     private string SuppName_Verif = null!; //Non-nullable, nécessaire
     private string Cont_FirstName_Verif = null!; //Non-nullable, nécessaire
     private string Cont_LastName_Verif = null!; //Non-nullable, nécessaire
-    private string Cont_Phone_Verif;
-    private string Cont_Email_Verif;
+    private string Cont_Email_Verif = null;
+    private string Cont_Phone_Verif = null;
 
     #region Propriétés de données (FIELD - CHAMPS)
     //0 : Validation ID 
     public int Id {
         get { return this.id_Verif; } //prend variable privé
-        set {   //En utilisant notre Methode, check si pas Positif (! + ValidateP).
-            if (!ValidateId(value)) { throw new ArgumentOutOfRangeException("Id", "Id is negative. Id must be greater than or equal to 0."); }
-            this.id_Verif = value; //< Sinon = valide le input 
-        }
+        set { //En utilisant notre Methode, check si pas Positif (! + ValidateP).
+              if (!ValidateId(value)) { throw new ArgumentOutOfRangeException("Id", "Id is negative. Id must be greater than or equal to 0."); }
+              //< Sinon = valide le input 
+              this.id_Verif = value; 
+            }
     }
     //1 : Validation SupplierName
     public string SupplierName {
-        get;
-        set;
+        get { return this.SuppName_Verif; }
+        set { if (!ValidateSupplierName(value)) { throw new ArgumentOutOfRangeException("Id", $"Supplier Company Name is too long. Maximum number of characters allowed is {SUPPLIER_NAME_MAXLENGHT} chars."); }
+              this.SuppName_Verif = value;
+            }
     }
     //2 + 3 : Validation Noms du Contacte
     public string Contact_FirstName {
-        get;
-        set;
+        get { return this.Cont_FirstName_Verif; }
+        set {
+              if (!ValidateContactName(value)) { throw new ArgumentOutOfRangeException("Id", $"First Name inputed is too long. Maximum number of characters allowed is {CONTACT_NAME_MAXLENGHT} chars.\nPlease respect the limit norms, thank you."); }
+              this.Cont_FirstName_Verif = value;
+            }
     }
     public string Contact_LastName {
-        get;
-        set;
+        get { return this.Cont_LastName_Verif; }
+        set {
+              if (!ValidateContactName(value)) { throw new ArgumentOutOfRangeException("Id", $"Last Name inputed is too long. Maximum number of characters allowed is {CONTACT_NAME_MAXLENGHT} chars.\nPlease respect the limit, thank you."); }
+              this.Cont_LastName_Verif = value;
+            }
     }
     //4 : Contact Courriel Elec
-    public string Contact_Email {
-        get;
-        set;
+    public string? Contact_Email {
+        get { return this.Cont_Email_Verif; }
+        set { if (!ValidateContactEmail(value)) { throw new ArgumentOutOfRangeException("Id", $"Email is too long. Make it less than {CONTACT_EMAIL_MAXLENGHT} characters, please."); }
+              this.Cont_Email_Verif = value;
+            }
     }
     //5 : Contact Téléphone
-    public string Contact_Phone {
-        get;
-        set;
+    public string? Contact_Phone {
+        get { return this.Cont_Phone_Verif; }
+        set { if (!ValidateContactPhone(value)) { throw new ArgumentOutOfRangeException("Id", $"Phone Lenght is too long. Make it less than {CONTACT_PHONE_MAXLENGHT - 3} numbers, please."); }
+              this.Cont_Phone_Verif = value;
+            }
     }
     //6 - 8 : Variables Dates
     public DateTime DateCreated { get; set; }
@@ -59,7 +72,9 @@ public class Supplier {
     public DateTime? DateModified { get; set; }
     public DateTime? DateDeleted { get; set; }
     //9 : LISTE pour gérer multiples produits qui peuventêtre liés a un Fournisseur
-    public List<Product> Products_List { get; set; }
+    public List<Product> Products_List { get; set; } = new List<Product>();
+    public byte[] RowVersion { get; set; } = null!; //Non-nullable, nécessaire
+
     #endregion Propriétés de données
 
     #region Propriétés de navigation
@@ -69,26 +84,43 @@ public class Supplier {
     ///Constructors
 
     // Const orienté Création Manuelle/UI par employé de bureau
-    public Supplier(int ID, string nomFournisseur, string prenom, string nom, string phone, string courriel) {
-
-
+    public Supplier(int ID, string nomFournisseur, string prenom, string nom, string? courriel, string? phone) 
+    {
+        this.Id = ID;
+        this.SupplierName = nomFournisseur;
+        this.Contact_FirstName = prenom;
+        this.Contact_LastName = nom;
+        this.Contact_Email = courriel;
+        this.Contact_Phone = phone;
     }
-    // ToDo : 2me Const (valeurs automaticaques created by DataBase SQL)
-
-    // ToDo : Const Orienté Entity-Framework
-
+    // 2me Const : Const Orienté Entity-Framework (valeurs automaticaques created by DataBase SQL)
+    public Supplier(int ID, 
+                    string nomFournisseur, 
+                    string prenom, 
+                    string nom, 
+                    string? courriel, 
+                    string? phone, 
+                    DateTime dateCreated, 
+                    DateTime? dateModified, 
+                    DateTime? dateDeleted, 
+                    byte[] rowVersion) 
+                    : this(ID, nomFournisseur, prenom, nom, courriel, phone)
+    {
+        this.DateCreated = dateCreated;
+        this.DateModified = dateModified;
+        this.DateDeleted = dateDeleted;
+        this.RowVersion = rowVersion;
+    }
     #region MÉTHODES
     public static bool ValidateId(int Supplier_id) 
         { return Supplier_id >= 0; } //doit etre positif 
     public static bool ValidateSupplierName(string nom) 
         { return nom.Length <= SUPPLIER_NAME_MAXLENGHT; } //est elle <= que le maximum?
-    public static bool ValidateContact_FirstName(string nom) 
+    public static bool ValidateContactName(string nom) 
         { return nom.Length <= CONTACT_NAME_MAXLENGHT; }
-    public static bool ValidateContact_LastName(string Prenom) 
-        { return Prenom.Length <= CONTACT_NAME_MAXLENGHT; }
-    public static bool ValidateContactPhone(string telefone) 
-        { return telefone.Length <= CONTACT_PHONE_MAXLENGHT; }
     public static bool ValidateContactEmail(string email) 
         { return email.Length <= CONTACT_EMAIL_MAXLENGHT; }
+    public static bool ValidateContactPhone(string telefone) 
+        { return telefone.Length <= CONTACT_PHONE_MAXLENGHT; }
 }
     #endregion
