72c389b5b7ea114f7d33b5d7d503036a88921e81
Sergio Acosta <sergioalasalle@gmail.com>
2024-12-05 12:04:25 -0500

    Warehouse Service ajouté

---
+++ 420DA3_A24_Projet/Business/Services/WarehouseService.cs
@@ -0,0 +1,132 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using _420DA3_A24_Projet.DataAccess.DAOs;
+using _420DA3_A24_Projet.Presentation.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Services;
+internal class WarehouseService {
+
+    private WsysApplication parentApp;
+    private WarehouseDAO warehouseDAO;
+    private WarehouseView warehouseWindow;
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="parentApp"></param>
+    /// <param name="context"></param>
+    public WarehouseService(WsysApplication parentApp, AppDbContext context) {
+        this.parentApp = parentApp;
+        this.warehouseDAO = new WarehouseDAO(context);
+        this.warehouseWindow = new WarehouseView(parentApp);
+    }
+
+
+    #region METHODS
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <returns></returns>
+    public Warehouse? OpenWarehouseManagementWindowForCreation() {
+        Warehouse newEmptyWarehouse = (Warehouse) FormatterServices.GetUninitializedObject(typeof(Warehouse));
+        DialogResult result = this.warehouseWindow.OpenForCreation(newEmptyWarehouse);
+        return result == DialogResult.OK
+            ? this.warehouseWindow.CurrentEntityInstance
+            : null;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouseToEdit"></param>
+    /// <returns></returns>
+    public Warehouse OpenWarehouseManagementWindowForEdition(Warehouse warehouseToEdit) {
+        _ = this.warehouseWindow.OpenForModification(warehouseToEdit);
+        return warehouseToEdit;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouseToView"></param>
+    /// <returns></returns>
+    public Warehouse OpenWarehouseManagementWindowForVisualization(Warehouse warehouseToView) {
+        _ = this.warehouseWindow.OpenForDetailsView(warehouseToView);
+        return warehouseToView;
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouseToDelete"></param>
+    /// <returns></returns>
+    public Warehouse OpenWarehouseManagementWindowForDeletion(Warehouse warehouseToDelete) {
+        _ = this.warehouseWindow.OpenForDeletion(warehouseToDelete);
+        return warehouseToDelete;
+    }
+
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Warehouse? GetWarehouseById(int id) {
+        return this.warehouseDAO.GetById(id);
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouseName"></param>
+    /// <returns></returns>
+    public Warehouse? GetWarehouseByWarehouseName(string warehouseName) {
+        return this.warehouseDAO.GetByWarehouseName(warehouseName);
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="criterion"></param>
+    /// <returns></returns>
+    public List<Warehouse> SearchWarehouses(string criterion) {
+        return this.warehouseDAO.Search(criterion);
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouse"></param>
+    /// <returns></returns>
+    public Warehouse CreateWarehouseInDatabase(Warehouse warehouse) {
+        return this.warehouseDAO.Create(warehouse);
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouse"></param>
+    /// <returns></returns>
+    public Warehouse UpdateWarehouseInDatabase(Warehouse warehouse) {
+        return this.warehouseDAO.Update(warehouse);
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="warehouse"></param>
+    /// <returns></returns>
+    public Warehouse DeleteWarehouseFromDatabase(Warehouse warehouse) {
+        return this.warehouseDAO.Delete(warehouse);
+    }
+
+    #endregion
+
+}
--- 420DA3_A24_Projet/Presentation/Views/WarehouseView.cs
+++ 420DA3_A24_Projet/Presentation/Views/WarehouseView.cs
@@ -1,4 +1,6 @@
 ﻿using _420DA3_A24_Projet.Business;
+using _420DA3_A24_Projet.Business.Domain;
+using Project_Utilities.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,8 +15,52 @@ namespace _420DA3_A24_Projet.Presentation.Views;
 internal partial class WarehouseView : Form {
     private bool isInitialized = false;
     private WsysApplication app;
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    public Warehouse CurrentEntityInstance { get; private set; }
     public WarehouseView(WsysApplication application) {
         this.app = application;
         this.InitializeComponent();
     }
-}
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <returns></returns>
+    public DialogResult OpenForCreation(Warehouse instance) {
+        this.PreOpenSetup(instance, ViewActionsEnum.Creation, "Création d'un entrepot", "Créer");
+        return this.ShowDialog();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <returns></returns>
+    public DialogResult OpenForDetailsView(Warehouse instance) {
+        this.PreOpenSetup(instance, ViewActionsEnum.Visualization, "Détails d'un entrepot", "OK");
+        return this.ShowDialog();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <returns></returns>
+    public DialogResult OpenForModification(Warehouse instance) {
+        this.PreOpenSetup(instance, ViewActionsEnum.Edition, "Modifier un entrepot", "Enregistrer");
+        return this.ShowDialog();
+    }
+
+    /// <summary>
+    /// TODO @PROF : documenter
+    /// </summary>
+    /// <param name="instance"></param>
+    /// <returns></returns>
+    public DialogResult OpenForDeletion(Warehouse instance) {
+        this.PreOpenSetup(instance, ViewActionsEnum.Deletion, "Supprimer un entrepot", "Supprimer");
+        return this.ShowDialog();
+    }
