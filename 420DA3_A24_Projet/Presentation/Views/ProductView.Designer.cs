8d7a6446c750f8ec838ac1db1d1d436fa3ca19cd
Marco <marc0mourr96@gmail.com>
2024-12-15 17:25:37 -0500

    SupplierView_Template

--- 420DA3_A24_Projet/Presentation/Views/ProductView.Designer.cs
+++ 420DA3_A24_Projet/Presentation/Views/ProductView.Designer.cs
@@ -28,19 +28,20 @@ partial class ProductView {
         this.Bottom_bar = new Panel();
         this.tblPanel_Main = new TableLayoutPanel();
         this.pnl_Main_Search = new Panel();
-        this.panel3 = new Panel();
-        this.panel1 = new Panel();
-        this.panel2 = new Panel();
-        this.tableLayoutPanel2 = new TableLayoutPanel();
-        this.tableLayoutPanel1 = new TableLayoutPanel();
+        this.pnl_Delete = new Panel();
+        this.tblPanel_SearchList = new TableLayoutPanel();
+        this.tblPanel_Confirm_Search = new TableLayoutPanel();
         this.pnl_Search_Titles = new Panel();
         this.pnl_Main_Details = new Panel();
-        this.groupBox1 = new GroupBox();
+        this.grpBox_Details = new GroupBox();
         this.pnl_Details_Titles = new Panel();
+        this.panel1 = new Panel();
+        this.pnl_Create = new Panel();
+        this.tblPanel_Modify = new TableLayoutPanel();
         this.tblPanel_Main.SuspendLayout();
         this.pnl_Main_Search.SuspendLayout();
-        this.panel1.SuspendLayout();
         this.pnl_Main_Details.SuspendLayout();
+        this.panel1.SuspendLayout();
         this.SuspendLayout();
         // 
         // Top_bar
@@ -78,10 +79,10 @@ partial class ProductView {
         // 
         // pnl_Main_Search
         // 
-        this.pnl_Main_Search.Controls.Add(this.panel3);
+        this.pnl_Main_Search.Controls.Add(this.pnl_Delete);
         this.pnl_Main_Search.Controls.Add(this.panel1);
-        this.pnl_Main_Search.Controls.Add(this.tableLayoutPanel2);
-        this.pnl_Main_Search.Controls.Add(this.tableLayoutPanel1);
+        this.pnl_Main_Search.Controls.Add(this.tblPanel_SearchList);
+        this.pnl_Main_Search.Controls.Add(this.tblPanel_Confirm_Search);
         this.pnl_Main_Search.Controls.Add(this.pnl_Search_Titles);
         this.pnl_Main_Search.Dock = DockStyle.Fill;
         this.pnl_Main_Search.Location = new Point(121, 3);
@@ -89,58 +90,41 @@ partial class ProductView {
         this.pnl_Main_Search.Size = new Size(616, 987);
         this.pnl_Main_Search.TabIndex = 0;
         // 
-        // panel3
+        // pnl_Delete
         // 
-        this.panel3.Dock = DockStyle.Right;
-        this.panel3.Location = new Point(324, 882);
-        this.panel3.Name = "panel3";
-        this.panel3.Size = new Size(292, 105);
-        this.panel3.TabIndex = 5;
+        this.pnl_Delete.Dock = DockStyle.Right;
+        this.pnl_Delete.Location = new Point(298, 882);
+        this.pnl_Delete.Name = "pnl_Delete";
+        this.pnl_Delete.Size = new Size(318, 105);
+        this.pnl_Delete.TabIndex = 5;
         // 
-        // panel1
+        // tblPanel_SearchList
         // 
-        this.panel1.Controls.Add(this.panel2);
-        this.panel1.Dock = DockStyle.Left;
-        this.panel1.Location = new Point(0, 882);
-        this.panel1.Name = "panel1";
-        this.panel1.Size = new Size(292, 105);
-        this.panel1.TabIndex = 3;
+        this.tblPanel_SearchList.ColumnCount = 3;
+        this.tblPanel_SearchList.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_SearchList.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 460F));
+        this.tblPanel_SearchList.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_SearchList.Dock = DockStyle.Top;
+        this.tblPanel_SearchList.Location = new Point(0, 225);
+        this.tblPanel_SearchList.Name = "tblPanel_SearchList";
+        this.tblPanel_SearchList.RowCount = 1;
+        this.tblPanel_SearchList.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_SearchList.Size = new Size(616, 657);
+        this.tblPanel_SearchList.TabIndex = 2;
         // 
-        // panel2
+        // tblPanel_Confirm_Search
         // 
-        this.panel2.Dock = DockStyle.Right;
-        this.panel2.Location = new Point(0, 0);
-        this.panel2.Name = "panel2";
-        this.panel2.Size = new Size(292, 105);
-        this.panel2.TabIndex = 4;
-        // 
-        // tableLayoutPanel2
-        // 
-        this.tableLayoutPanel2.ColumnCount = 3;
-        this.tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-        this.tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 460F));
-        this.tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-        this.tableLayoutPanel2.Dock = DockStyle.Top;
-        this.tableLayoutPanel2.Location = new Point(0, 225);
-        this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-        this.tableLayoutPanel2.RowCount = 1;
-        this.tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-        this.tableLayoutPanel2.Size = new Size(616, 657);
-        this.tableLayoutPanel2.TabIndex = 2;
-        // 
-        // tableLayoutPanel1
-        // 
-        this.tableLayoutPanel1.ColumnCount = 3;
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 350F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-        this.tableLayoutPanel1.Dock = DockStyle.Top;
-        this.tableLayoutPanel1.Location = new Point(0, 125);
-        this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-        this.tableLayoutPanel1.RowCount = 1;
-        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-        this.tableLayoutPanel1.Size = new Size(616, 100);
-        this.tableLayoutPanel1.TabIndex = 1;
+        this.tblPanel_Confirm_Search.ColumnCount = 3;
+        this.tblPanel_Confirm_Search.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Confirm_Search.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 350F));
+        this.tblPanel_Confirm_Search.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Confirm_Search.Dock = DockStyle.Top;
+        this.tblPanel_Confirm_Search.Location = new Point(0, 125);
+        this.tblPanel_Confirm_Search.Name = "tblPanel_Confirm_Search";
+        this.tblPanel_Confirm_Search.RowCount = 1;
+        this.tblPanel_Confirm_Search.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_Confirm_Search.Size = new Size(616, 100);
+        this.tblPanel_Confirm_Search.TabIndex = 1;
         // 
         // pnl_Search_Titles
         // 
@@ -152,7 +136,8 @@ partial class ProductView {
         // 
         // pnl_Main_Details
         // 
-        this.pnl_Main_Details.Controls.Add(this.groupBox1);
+        this.pnl_Main_Details.Controls.Add(this.tblPanel_Modify);
+        this.pnl_Main_Details.Controls.Add(this.grpBox_Details);
         this.pnl_Main_Details.Controls.Add(this.pnl_Details_Titles);
         this.pnl_Main_Details.Dock = DockStyle.Fill;
         this.pnl_Main_Details.Location = new Point(743, 3);
@@ -160,14 +145,14 @@ partial class ProductView {
         this.pnl_Main_Details.Size = new Size(616, 987);
         this.pnl_Main_Details.TabIndex = 1;
         // 
-        // groupBox1
+        // grpBox_Details
         // 
-        this.groupBox1.Location = new Point(3, 131);
-        this.groupBox1.Name = "groupBox1";
-        this.groupBox1.Size = new Size(580, 853);
-        this.groupBox1.TabIndex = 1;
-        this.groupBox1.TabStop = false;
-        this.groupBox1.Text = "Details";
+        this.grpBox_Details.Location = new Point(15, 131);
+        this.grpBox_Details.Name = "grpBox_Details";
+        this.grpBox_Details.Size = new Size(585, 745);
+        this.grpBox_Details.TabIndex = 1;
+        this.grpBox_Details.TabStop = false;
+        this.grpBox_Details.Text = "Details";
         // 
         // pnl_Details_Titles
         // 
@@ -176,6 +161,37 @@ partial class ProductView {
         this.pnl_Details_Titles.Name = "pnl_Details_Titles";
         this.pnl_Details_Titles.Size = new Size(616, 125);
         this.pnl_Details_Titles.TabIndex = 0;
+        // 
+        // panel1
+        // 
+        this.panel1.Controls.Add(this.pnl_Create);
+        this.panel1.Dock = DockStyle.Left;
+        this.panel1.Location = new Point(0, 882);
+        this.panel1.Name = "panel1";
+        this.panel1.Size = new Size(292, 105);
+        this.panel1.TabIndex = 3;
+        // 
+        // pnl_Create
+        // 
+        this.pnl_Create.Dock = DockStyle.Left;
+        this.pnl_Create.Location = new Point(0, 0);
+        this.pnl_Create.Name = "pnl_Create";
+        this.pnl_Create.Size = new Size(297, 105);
+        this.pnl_Create.TabIndex = 4;
+        // 
+        // tblPanel_Modify
+        // 
+        this.tblPanel_Modify.ColumnCount = 3;
+        this.tblPanel_Modify.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Modify.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
+        this.tblPanel_Modify.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Modify.Dock = DockStyle.Bottom;
+        this.tblPanel_Modify.Location = new Point(0, 882);
+        this.tblPanel_Modify.Name = "tblPanel_Modify";
+        this.tblPanel_Modify.RowCount = 1;
+        this.tblPanel_Modify.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_Modify.Size = new Size(616, 105);
+        this.tblPanel_Modify.TabIndex = 4;
         // 
         // ProductView
         // 
@@ -191,8 +207,8 @@ partial class ProductView {
         this.Text = "Products";
         this.tblPanel_Main.ResumeLayout(false);
         this.pnl_Main_Search.ResumeLayout(false);
-        this.panel1.ResumeLayout(false);
         this.pnl_Main_Details.ResumeLayout(false);
+        this.panel1.ResumeLayout(false);
         this.ResumeLayout(false);
     }
 
@@ -205,10 +221,11 @@ partial class ProductView {
     private Panel pnl_Main_Details;
     private Panel pnl_Search_Titles;
     private Panel pnl_Details_Titles;
+    private TableLayoutPanel tblPanel_SearchList;
+    private TableLayoutPanel tblPanel_Confirm_Search;
+    private Panel pnl_Delete;
+    private GroupBox grpBox_Details;
     private Panel panel1;
-    private TableLayoutPanel tableLayoutPanel2;
-    private TableLayoutPanel tableLayoutPanel1;
-    private Panel panel3;
-    private Panel panel2;
-    private GroupBox groupBox1;
+    private Panel pnl_Create;
+    private TableLayoutPanel tblPanel_Modify;
 }
--- 420DA3_A24_Projet/Presentation/Views/SupplierView.Designer.cs
+++ 420DA3_A24_Projet/Presentation/Views/SupplierView.Designer.cs
@@ -24,11 +24,195 @@ partial class SupplierView {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "SupplierView";
+        this.Top_Bar = new Panel();
+        this.Bottom_Bar = new Panel();
+        this.tblPanel_Main = new TableLayoutPanel();
+        this.pnl_Main_Search = new Panel();
+        this.pnl_Main_Details = new Panel();
+        this.pnl_Search_Titles = new Panel();
+        this.tblPanel_Confirm_Search = new TableLayoutPanel();
+        this.pnl_Details_Titles = new Panel();
+        this.tblPanel_SearchList = new TableLayoutPanel();
+        this.pnl_Delete = new Panel();
+        this.pnl_Create = new Panel();
+        this.grpBox_Details = new GroupBox();
+        this.tblPanel_Modify = new TableLayoutPanel();
+        this.tblPanel_Main.SuspendLayout();
+        this.pnl_Main_Search.SuspendLayout();
+        this.pnl_Main_Details.SuspendLayout();
+        this.SuspendLayout();
+        // 
+        // Top_Bar
+        // 
+        this.Top_Bar.Dock = DockStyle.Top;
+        this.Top_Bar.Location = new Point(0, 0);
+        this.Top_Bar.Name = "Top_Bar";
+        this.Top_Bar.Size = new Size(1182, 80);
+        this.Top_Bar.TabIndex = 0;
+        // 
+        // Bottom_Bar
+        // 
+        this.Bottom_Bar.Dock = DockStyle.Bottom;
+        this.Bottom_Bar.Location = new Point(0, 873);
+        this.Bottom_Bar.Name = "Bottom_Bar";
+        this.Bottom_Bar.Size = new Size(1182, 80);
+        this.Bottom_Bar.TabIndex = 1;
+        // 
+        // tblPanel_Main
+        // 
+        this.tblPanel_Main.ColumnCount = 4;
+        this.tblPanel_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 8F));
+        this.tblPanel_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 42F));
+        this.tblPanel_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 42F));
+        this.tblPanel_Main.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 8F));
+        this.tblPanel_Main.Controls.Add(this.pnl_Main_Search, 1, 0);
+        this.tblPanel_Main.Controls.Add(this.pnl_Main_Details, 2, 0);
+        this.tblPanel_Main.Dock = DockStyle.Fill;
+        this.tblPanel_Main.Location = new Point(0, 80);
+        this.tblPanel_Main.Name = "tblPanel_Main";
+        this.tblPanel_Main.RowCount = 1;
+        this.tblPanel_Main.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_Main.Size = new Size(1182, 793);
+        this.tblPanel_Main.TabIndex = 2;
+        // 
+        // pnl_Main_Search
+        // 
+        this.pnl_Main_Search.Controls.Add(this.pnl_Delete);
+        this.pnl_Main_Search.Controls.Add(this.pnl_Create);
+        this.pnl_Main_Search.Controls.Add(this.tblPanel_SearchList);
+        this.pnl_Main_Search.Controls.Add(this.tblPanel_Confirm_Search);
+        this.pnl_Main_Search.Controls.Add(this.pnl_Search_Titles);
+        this.pnl_Main_Search.Dock = DockStyle.Fill;
+        this.pnl_Main_Search.Location = new Point(97, 3);
+        this.pnl_Main_Search.Name = "pnl_Main_Search";
+        this.pnl_Main_Search.Size = new Size(490, 787);
+        this.pnl_Main_Search.TabIndex = 0;
+        // 
+        // pnl_Main_Details
+        // 
+        this.pnl_Main_Details.Controls.Add(this.tblPanel_Modify);
+        this.pnl_Main_Details.Controls.Add(this.grpBox_Details);
+        this.pnl_Main_Details.Controls.Add(this.pnl_Details_Titles);
+        this.pnl_Main_Details.Dock = DockStyle.Fill;
+        this.pnl_Main_Details.Location = new Point(593, 3);
+        this.pnl_Main_Details.Name = "pnl_Main_Details";
+        this.pnl_Main_Details.Size = new Size(490, 787);
+        this.pnl_Main_Details.TabIndex = 1;
+        // 
+        // pnl_Search_Titles
+        // 
+        this.pnl_Search_Titles.Dock = DockStyle.Top;
+        this.pnl_Search_Titles.Location = new Point(0, 0);
+        this.pnl_Search_Titles.Name = "pnl_Search_Titles";
+        this.pnl_Search_Titles.Size = new Size(490, 115);
+        this.pnl_Search_Titles.TabIndex = 0;
+        // 
+        // tblPanel_Confirm_Search
+        // 
+        this.tblPanel_Confirm_Search.ColumnCount = 3;
+        this.tblPanel_Confirm_Search.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Confirm_Search.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
+        this.tblPanel_Confirm_Search.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Confirm_Search.Dock = DockStyle.Top;
+        this.tblPanel_Confirm_Search.Location = new Point(0, 115);
+        this.tblPanel_Confirm_Search.Name = "tblPanel_Confirm_Search";
+        this.tblPanel_Confirm_Search.RowCount = 1;
+        this.tblPanel_Confirm_Search.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_Confirm_Search.Size = new Size(490, 93);
+        this.tblPanel_Confirm_Search.TabIndex = 1;
+        // 
+        // pnl_Details_Titles
+        // 
+        this.pnl_Details_Titles.Dock = DockStyle.Top;
+        this.pnl_Details_Titles.Location = new Point(0, 0);
+        this.pnl_Details_Titles.Name = "pnl_Details_Titles";
+        this.pnl_Details_Titles.Size = new Size(490, 115);
+        this.pnl_Details_Titles.TabIndex = 1;
+        // 
+        // tblPanel_SearchList
+        // 
+        this.tblPanel_SearchList.ColumnCount = 3;
+        this.tblPanel_SearchList.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_SearchList.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 350F));
+        this.tblPanel_SearchList.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_SearchList.Dock = DockStyle.Top;
+        this.tblPanel_SearchList.Location = new Point(0, 208);
+        this.tblPanel_SearchList.Name = "tblPanel_SearchList";
+        this.tblPanel_SearchList.RowCount = 1;
+        this.tblPanel_SearchList.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_SearchList.Size = new Size(490, 490);
+        this.tblPanel_SearchList.TabIndex = 2;
+        // 
+        // pnl_Delete
+        // 
+        this.pnl_Delete.Dock = DockStyle.Right;
+        this.pnl_Delete.Location = new Point(253, 698);
+        this.pnl_Delete.Name = "pnl_Delete";
+        this.pnl_Delete.Size = new Size(237, 89);
+        this.pnl_Delete.TabIndex = 7;
+        // 
+        // pnl_Create
+        // 
+        this.pnl_Create.Dock = DockStyle.Left;
+        this.pnl_Create.Location = new Point(0, 698);
+        this.pnl_Create.Name = "pnl_Create";
+        this.pnl_Create.Size = new Size(254, 89);
+        this.pnl_Create.TabIndex = 6;
+        // 
+        // grpBox_Details
+        // 
+        this.grpBox_Details.Location = new Point(15, 127);
+        this.grpBox_Details.Name = "grpBox_Details";
+        this.grpBox_Details.Size = new Size(456, 547);
+        this.grpBox_Details.TabIndex = 2;
+        this.grpBox_Details.TabStop = false;
+        this.grpBox_Details.Text = "Details";
+        // 
+        // tblPanel_Modify
+        // 
+        this.tblPanel_Modify.ColumnCount = 3;
+        this.tblPanel_Modify.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Modify.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
+        this.tblPanel_Modify.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.tblPanel_Modify.Dock = DockStyle.Bottom;
+        this.tblPanel_Modify.Location = new Point(0, 698);
+        this.tblPanel_Modify.Name = "tblPanel_Modify";
+        this.tblPanel_Modify.RowCount = 1;
+        this.tblPanel_Modify.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tblPanel_Modify.Size = new Size(490, 89);
+        this.tblPanel_Modify.TabIndex = 3;
+        // 
+        // SupplierView
+        // 
+        this.AutoScaleDimensions = new SizeF(8F, 20F);
+        this.AutoScaleMode = AutoScaleMode.Font;
+        this.ClientSize = new Size(1182, 953);
+        this.Controls.Add(this.tblPanel_Main);
+        this.Controls.Add(this.Bottom_Bar);
+        this.Controls.Add(this.Top_Bar);
+        this.MinimumSize = new Size(1200, 1000);
+        this.Name = "SupplierView";
+        this.StartPosition = FormStartPosition.CenterScreen;
+        this.Text = "Suppliers";
+        this.tblPanel_Main.ResumeLayout(false);
+        this.pnl_Main_Search.ResumeLayout(false);
+        this.pnl_Main_Details.ResumeLayout(false);
+        this.ResumeLayout(false);
     }
 
     #endregion
+
+    private Panel Top_Bar;
+    private Panel Bottom_Bar;
+    private TableLayoutPanel tblPanel_Main;
+    private Panel pnl_Main_Search;
+    private Panel pnl_Main_Details;
+    private TableLayoutPanel tblPanel_Confirm_Search;
+    private Panel pnl_Search_Titles;
+    private Panel pnl_Details_Titles;
+    private TableLayoutPanel tblPanel_SearchList;
+    private Panel pnl_Delete;
+    private Panel pnl_Create;
+    private GroupBox grpBox_Details;
+    private TableLayoutPanel tblPanel_Modify;
 }
