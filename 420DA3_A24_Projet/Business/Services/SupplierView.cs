d38e3474eea48b2e0a477d9ba48ba7fddeb59365
Marco <marc0mourr96@gmail.com>
2024-12-15 23:30:58 -0500

    mini changement

--- 420DA3_A24_Projet/Business/Services/SupplierView.cs
+++ 420DA3_A24_Projet/Business/Services/SupplierView.cs
@@ -1,4 +1,9 @@
 ﻿namespace _420DA3_A24_Projet.Business.Services;
 
 internal class SupplierView {
+    private WsysApplication parentApp;
+
+    public SupplierView(WsysApplication parentApp) {
+        this.parentApp = parentApp;
+    }
 }
